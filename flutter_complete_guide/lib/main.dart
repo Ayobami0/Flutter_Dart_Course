29f59c80efbebb0987550d826be4a19311cfcadf
Ayobami <oludemiayobami@gmail.com>
2022-10-13 17:35:43 +0100

    L38

--- flutter_complete_guide/lib/main.dart
+++ flutter_complete_guide/lib/main.dart
@@ -7,18 +7,18 @@ void main() {
 class MyApp extends StatefulWidget {
   @override
   State<StatefulWidget> createState() {
-    return MyAppState();
+    return _MyAppState();
   }
 }
 
-class MyAppState extends State<MyApp> {
-  var questionIndex = 0;
+class _MyAppState extends State<MyApp> {
+  var _questionIndex = 0;
 
-  void answerQuestion(){
+  void _answerQuestion(){
       setState(() {
-        questionIndex++;
+        _questionIndex++;
       });
-      print(questionIndex);
+      print(_questionIndex);
     }
 
   @override
@@ -34,10 +34,10 @@ class MyAppState extends State<MyApp> {
         ),
         body: Column(
           children: <Widget>[
-            Text(questions.elementAt(questionIndex)),
+            Text(questions.elementAt(_questionIndex)),
             RaisedButton(
               child: Text('Answer 1'),
-              onPressed: answerQuestion,
+              onPressed: _answerQuestion,
             ),
             RaisedButton(
               child: Text('Answer 2'),
