c5d64c47edeae287ac342e3fcbe0b0d97d44c24a
Ayobami <oludemiayobami@gmail.com>
2022-10-13 14:25:07 +0100

    L26

--- flutter_complete_guide/lib/main.dart
+++ flutter_complete_guide/lib/main.dart
@@ -1,14 +1,14 @@
 import "package:flutter/material.dart";
 
 void main() {
-  
+  runApp(MyApp());  
 }
 
 class MyApp extends StatelessWidget {
     @override
-      Widget build(BuildContext context) {
-        return MaterialApp(
-          home: Text('Hello!'),
-        );
-      }
+    Widget build(BuildContext context) {
+      return MaterialApp(
+        home: Text('Hello!'),
+      );
+    }
   }
