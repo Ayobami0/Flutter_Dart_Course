0bbaa40dd75aa383eabfc13d0cd4d65dfcf6ad0f
Ayobami <oludemiayobami@gmail.com>
2022-12-06 15:55:12 +0100

    L55

--- flutter_complete_guide/lib/result.dart
+++ flutter_complete_guide/lib/result.dart
@@ -2,18 +2,18 @@ import 'package:flutter/material.dart';
 
 class Result extends StatelessWidget {
   final int resultScore;
-  final VoidCallback reset; 
+  final VoidCallback reset;
 
   Result({required this.resultScore, required this.reset});
 
-  String get resultPhrase{
+  String get resultPhrase {
     String resultText = 'You did it';
-    if (resultScore < 7){
+    if (resultScore < 7) {
       resultText = 'You are .....strange?!';
-    } else if (resultScore <= 12){
-        resultText = 'Pretty Likeable';
-    } else if (resultScore <= 16){
-        resultText = 'You are awesome and innocent';
+    } else if (resultScore <= 12) {
+      resultText = 'Pretty Likeable';
+    } else if (resultScore <= 16) {
+      resultText = 'You are awesome and innocent';
     }
     return resultText;
   }
@@ -23,15 +23,18 @@ class Result extends StatelessWidget {
     return Column(
       children: [
         Center(
-          child: Text(resultPhrase,
-            textAlign: TextAlign.center,
-            style: TextStyle(
-              fontSize: 36,
-              fontWeight: FontWeight.bold),
-              )
-          ),
-        FlatButton(onPressed: reset, child: Text('Reset Text', style: TextStyle(color: Colors.blue),))
+            child: Text(
+          resultPhrase,
+          textAlign: TextAlign.center,
+          style: TextStyle(fontSize: 36, fontWeight: FontWeight.bold),
+        )),
+        TextButton(
+            onPressed: reset,
+            child: Text(
+              'Reset Text',
+              style: TextStyle(color: Colors.blue),
+            ))
       ],
-    ); 
+    );
   }
 }
